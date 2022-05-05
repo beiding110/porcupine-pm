a08c590ac861dbcdfb6b06b441574fbf3af7ea9e
yzh <beiding110@gmail.com>
2022-05-05 19:24:07 +0800

    【新增】项目工作量引入日历；

---
+++ fe/src/components-sys/tui-calendar.vue
@@ -0,0 +1,138 @@
+<template>
+    <div class="tui-calendar">
+        <div class="menu">
+            <el-form class="menu-navi" inline size="small">
+                <el-form-item>
+                    <el-button size="small" @click="shiftTo('prev')" icon="el-icon-arrow-left"></el-button>
+                    <el-button size="small" @click="backToToday">今日</el-button>
+                    <el-button size="small" @click="shiftTo('next')" icon="el-icon-arrow-right"></el-button>
+                </el-form-item>
+                
+                <el-form-item>
+                    <el-date-picker
+                        v-model="date"
+                        type="month"
+                        placeholder="选择月"
+                        size="small"
+                        format="yyyy-MM"
+                        value-format="yyyy-MM"
+                        @change="dateSel"
+                    ></el-date-picker>
+                </el-form-item>
+            </el-form>
+        </div>
+
+        <div id="calendar" ref="calendar"></div>
+    </div>
+</template>
+
+<script>
+import Calendar from 'tui-calendar'; /* ES6 */
+import 'tui-calendar/dist/tui-calendar.css';
+
+export default {
+    props: {
+        schedule: {
+            type: Array,
+            default: () => [],
+        },
+        props: {
+            type: Object,
+            default: () => ({}),
+        },
+    },
+    data() {
+        return {
+            calendar: null,
+
+            date: new Date().pattern('yyyy-MM'),
+        };
+    },
+    watch: {
+        schedule: {
+            handler(n) {
+                var scheduleRebuild = this.rebuildSchedule(n);
+
+                try{
+                    this.calendar.clear();
+
+                    this.calendar.createSchedules(scheduleRebuild);
+                } catch(e) {
+                    // e
+                }
+            },
+            deep: true,
+            immediate: true,
+        }
+    },
+    methods: {
+        backToToday() {
+            this.calendar.today();
+
+            this.date = new Date().pattern('yyyy-MM');
+        },
+        shiftTo(type) {
+            this.calendar[type]();
+            
+            var _date = this.calendar.getDate()._date;
+
+            this.date = new Date(_date).pattern('yyyy-MM');
+        },
+        dateSel(e) {
+            this.calendar.setDate(new Date(e));
+        },
+        rebuildSchedule(arr) {
+            function propFactory(key, item) {
+                var keyInProps = this.props[key];
+
+                if (typeof keyInProps === 'function') {
+                    return keyInProps(item);
+                }
+
+                return item[keyInProps];
+            }
+
+            return arr.map(item => ({
+                id: propFactory.call(this, 'id', item),
+                title: propFactory.call(this, 'title', item),
+                start: propFactory.call(this, 'start', item),
+                end: propFactory.call(this, 'end', item),
+                bgColor: propFactory.call(this, 'bgColor', item),
+                category: 'allday',
+                isAllDay: true,
+                color: '#fff',
+            }));
+        }
+    },
+    mounted() {
+        this.calendar = new Calendar('#calendar', {
+            defaultView: 'month',
+            taskView: true,
+            isReadOnly: true,
+            month: {
+                daynames: ['日', '一', '二', '三', '四', '五', '六'],
+                startDayOfWeek: 0,
+                narrowWeekend: true
+            },
+            week: {
+                daynames: ['日', '一', '二', '三', '四', '五', '六'],
+                startDayOfWeek: 0,
+                narrowWeekend: true
+            }
+        });
+    },
+    beforeDestroy() {
+        this.calendar.destroy();
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    .tui-calendar{
+        .menu{
+            .menu-navi{
+                
+            }
+        }
+    }
+</style>
--- fe/src/views/teamwork/task-report/list-project.vue
+++ fe/src/views/teamwork/task-report/list-project.vue
@@ -1,65 +1,79 @@
 <template>
-    <el-card>
-        <my-search 
-        v-model="pgData" 
-        @search="queryData"
-        time-start-placeholder="工作日早至"
-        time-end-placeholder="工作日晚至"
-        >
-            <el-form-item>
-                <my-select 
-                    placeholder="成员"
-                    v-model="pgData.member" 
-                    :props="{label:'name', value:'_id'}"
-                    :url="`/projectmember/list?procode=${$route.params.procode}`"
-                ></my-select>
-            </el-form-item>
+    <el-row :gutter="10">
+        <el-col :span="12">
+            <el-card>
+                <my-search 
+                v-model="pgData" 
+                @search="queryData"
+                time-start-placeholder="工作日早至"
+                time-end-placeholder="工作日晚至"
+                >
+                    <el-form-item>
+                        <my-select 
+                            placeholder="成员"
+                            v-model="pgData.member" 
+                            :props="{label:'name', value:'_id'}"
+                            :url="`/projectmember/list?procode=${$route.params.procode}`"
+                        ></my-select>
+                    </el-form-item>
 
-            <span slot="title"></span>
-        </my-search>
+                    <span slot="title"></span>
+                </my-search>
 
-        <my-table
-        :data="tableData"
-        >
-            <el-table-column label="成员">
-                <template slot-scope="scope">
-                    {{scope.row.member.name}}
-                </template>
-            </el-table-column>
+                <my-table
+                :data="tableData"
+                >
+                    <el-table-column label="成员">
+                        <template slot-scope="scope">
+                            {{scope.row.member.name}}
+                        </template>
+                    </el-table-column>
 
-            <el-table-column label="任务">
-                <template slot-scope="scope">
-                    {{scope.row.taskcode.title}}
-                </template>
-            </el-table-column>
+                    <el-table-column label="任务">
+                        <template slot-scope="scope">
+                            {{scope.row.taskcode.title}}
+                        </template>
+                    </el-table-column>
 
-            <el-table-column label="工作日" prop="reporttime"></el-table-column>
-            <el-table-column label="工时" prop="tasktime"></el-table-column>
+                    <el-table-column label="工作日" prop="reporttime"></el-table-column>
+                    <el-table-column label="工时" prop="tasktime"></el-table-column>
 
-            <el-table-column label="操作" width="100px">
-                <template slot-scope="scope">
-                    <el-button type="text" icon="el-icon-edit" @click="editHandler(scope.row)">编辑</el-button>
-                </template>
-            </el-table-column>
-        </my-table>
+                    <el-table-column label="操作" width="100px">
+                        <template slot-scope="scope">
+                            <el-button type="text" icon="el-icon-edit" @click="editHandler(scope.row)">编辑</el-button>
+                        </template>
+                    </el-table-column>
+                </my-table>
 
-        <my-dialog
-        v-model="dialogVisible"
-        title="调整工作量"
-        width="700px"
-        >
-            <form-page 
-                v-if="dialogVisible"
-                :data="form"
-                @cancle="dialogClose"
-            ></form-page>
-        </my-dialog>
-    </el-card>
+                <my-dialog
+                v-model="dialogVisible"
+                title="调整工作量"
+                width="700px"
+                >
+                    <form-page 
+                        v-if="dialogVisible"
+                        :data="form"
+                        @cancle="dialogClose"
+                    ></form-page>
+                </my-dialog>
+            </el-card>
+        </el-col>
+        <el-col :span="12">
+            <el-card>
+                <tui-calendar
+                    :schedule="tableData"
+                    :props="{id: '_id', title: (item) => `${item.member.name}:${item.taskcode.title}`, start: 'reporttime', end: 'reporttime', bgColor: item => `#${item.member._id.slice(-7, -1)}`}"
+                ></tui-calendar>
+            </el-card>
+        </el-col>
+    </el-row>
 </template>
 
 <script>
 import LIST_MIXIN from '@mixins/list-page';
 import DIALOG_LIST_MIXIN from '@mixins/dialog-list-page';
+
+import TuiCalendar from '@components-sys/tui-calendar';
 
 import FormPage from './form';
 
@@ -67,6 +81,7 @@ export default {
     mixins: [LIST_MIXIN, DIALOG_LIST_MIXIN],
     components: {
         FormPage,
+        TuiCalendar,
     },
     data() {
         return {
