3d99aaa238b8d6cbe2a8266a8998c569b9f231c8
yzh <beiding110@gmail.com>
2022-05-09 11:20:20 +0800

    【新增】热度图组件；

--- fe/src/views/teamwork/home/index.vue
+++ fe/src/views/teamwork/home/index.vue
@@ -3,15 +3,31 @@
         <el-card>
             <cascader-all-member></cascader-all-member>
         </el-card>
+
+        <hotspot-map></hotspot-map>
     </div>
 </template>
 
 <script>
 import CascaderAllMember from '../task-report/components/cascader-all-member';
+import HotspotMap from './components/hotspot-map';
 
 export default {
     components: {
         CascaderAllMember,
+        HotspotMap,
+    },
+    data() {
+        return {
+
+        }
+    },
+    created() {
+        
     },
 }
 </script>
+
+<style lang="scss" scoped>
+
+</style>
