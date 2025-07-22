d81c3207d09487d6f1309f7b3a76be9b84743aa0
杨子豪 <beiding110@gmail.com>
2025-07-22 16:15:33 +0800

    feat: 优化时间轴组件；

--- fe/src/components/my/time-line.vue
+++ fe/src/components/my/time-line.vue
@@ -1,15 +1,15 @@
 <template>
-    <div 
-        ref="_visualization" 
-        style="position:relative;" 
-        class="my__timeline" 
-        v-loading="loadingController" 
+    <div
+        ref="_visualization"
+        style="position: relative"
+        class="my__timeline"
+        v-loading="loadingController"
         element-loading-background="rgba(0, 0, 0, 0)"
     ></div>
 </template>
 
 <script>
-const vis = require("vis-timeline/standalone");
+const vis = require('vis-timeline/standalone');
 
 export default {
     props: {
@@ -22,133 +22,186 @@ export default {
             default: () => [],
         },
     },
-    data () {
+    data() {
         return {
             timeline: null,
-            
-            loadingController: false
-        }
+
+            loadingController: false,
+        };
     },
     watch: {
         data: {
-            handler: function (curVal, oldVal) {
+            handler: function () {
                 this.init();
             },
         },
     },
     methods: {
         init: function () {
-            var that = this;
-
             this.loadingController = true;
 
             try {
                 this.timeline.destroy();
-            } catch (e) {}
+            } catch (e) {
+                // e
+            }
 
             // DOM element where the Timeline will be attached
-            if (!!this.data && this.data.length == 0) {
+            if (this.data && this.data.length === 0) {
                 this.loadingController = false;
 
                 return;
             }
 
             // Create a DataSet (allows two way data-binding)
-            var items = new vis.DataSet(this.data);
+            var items = new vis.DataSet(this.data),
+                groups;
 
             if (this.groups) {
-                var groups = new vis.DataSet(this.groups);
+                groups = new vis.DataSet(this.groups);
             }
 
             // Configuration for the Timeline
-            var options = {
+            const options = {
                 zoomMin: 1000 * 60 * 60 * 24 * 7 * 2,
                 // zoomMax: 1000 * 60 * 60 * 24 * 31 * 3,
-                onInitialDrawComplete: function () {
-                    that.loadingController = false;
+                onInitialDrawComplete: () => {
+                    this.loadingController = false;
                 },
-                // visibleFrameTemplate: function(item) {
-                //     if (item.visibleFrameTemplate) {
-                //         return item.visibleFrameTemplate;
-                //     }
+                visibleFrameTemplate: (item) => {
+                    if (item.visibleFrameTemplate) {
+                        return item.visibleFrameTemplate;
+                    }
 
-                //     if(!item.state){
-                //         return ''
-                //     }
+                    if (item.value) {
+                        let percentage = (Number(item.value) * 100).toFixed(0) + '%',
+                            progressClass = item.progressClass || '';
 
-                //     var percentage = (Number(item.value)*100).toFixed(0) + '%',
-                //         extraClassName = '';
+                        return `
+                            <div class="progress-wrapper">
+                                <div class="progress ${progressClass}" style="width: ${percentage}">
+                                    <label class="progress-label">${percentage}</label>
+                                </div>
+                            </div>
+                        `;
+                    }
 
-                //     var classSwitchObj = {
-                //         '0': 'info',
-                //         '1': 'warning',
-                //         '2': 'primary',
-                //     };
-
-                //     extraClassName = classSwitchObj[item.state] || '';
-
-                //     return `
-                //         <div class="progress-wrapper">
-                //             <div class="progress ${extraClassName}" style="width: ${percentage}">
-                //                 <label class="progress-label">${percentage}</label>
-                //             </div>
-                //         </div>
-                //     `;
-                // }
+                    return '';
+                },
             };
 
             // Create a Timeline
             this.timeline = new vis.Timeline(this.$refs._visualization, items, groups, options);
         },
-        updateData() {
-            
-        }
+        updateData() {},
     },
     mounted() {
         this.init();
     },
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-    @import '@css/var.scss';
+@import '@css/var.scss';
 
-    .my__timeline{
-        padding:1em;
+.my__timeline {
+    padding: 1em;
 
-        ::v-deep {
-            .progress-wrapper {width:100%; height:18px; text-align:center; position:relative; overflow:hidden;}
-            
+    ::v-deep {
+        .progress-wrapper {
+            width: 100%;
+            height: 18px;
+            text-align: center;
+            position: relative;
+            overflow: hidden;
+
             .progress {
-                height:100%; width:60%; position:absolute; left:0px; top:0px; background:$successColor; border-radius: 2px;
-                
-                &.info{background:$infoColor;}
-                &.error{background:$dangerColor;}
-                &.warning{background:$warningColor;}
+                height: 100%;
+                width: 60%;
+                position: absolute;
+                left: 0px;
+                top: 0px;
+                background: $successColor;
+                border-radius: 2px;
 
-                .progress-label {position:absolute; z-index:1; color:white;}
+                &.info {
+                    background: $infoColor;
+                }
+                &.error {
+                    background: $dangerColor;
+                }
+                &.warning {
+                    background: $warningColor;
+                }
+
+                .progress-label {
+                    position: absolute;
+                    z-index: 1;
+                    color: white;
+                }
             }
-            
-            .vis-timeline{border: none;}
+        }
 
-            .vis-current-time{background-color: red; z-index:2;}
+        .vis-timeline {
+            border: none;
 
-            .vis-item{
-                background-color: $primaryColor1; 
+            .vis-current-time {
+                background-color: red;
+                z-index: 2;
+            }
+
+            .vis-item {
+                background-color: $primaryColor1;
                 border: 1px solid $primaryColor;
-                
-                .vis-item-overflow{
-                    background-color: $primaryColor; 
-                    border-radius: 2px; 
+
+                .vis-item-overflow {
+                    background-color: $primaryColor;
+                    border-radius: 2px;
 
                     .vis-item-content {
                         color: white;
                     }
                 }
 
-                &.vis-selected{border: 1px solid $warningColor;}
+                &.info {
+                    background: $infoColor1;
+                    border: 1px solid $infoColor;
+
+                    .vis-item-overflow {
+                        background: $infoColor;
+                    }
+                }
+                &.success {
+                    background: $successColor1;
+                    border: 1px solid $successColor;
+
+                    .vis-item-overflow {
+                        background: $successColor;
+                    }
+                }
+                &.error {
+                    background: $dangerColor1;
+                    border: 1px solid $dangerColor;
+
+                    .vis-item-overflow {
+                        background: $dangerColor;
+                    }
+                }
+                &.warning {
+                    background: $warningColor1;
+                    border: 1px solid $warningColor;
+
+                    .vis-item-overflow {
+                        background: $warningColor;
+                    }
+                }
+
+                &.vis-selected {
+                    border: 1px solid $warningColor;
+                }
             }
         }
     }
+}
 </style>
